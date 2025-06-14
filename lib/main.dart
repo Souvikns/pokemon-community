9abafe5b71894288b44bdf321cb8fcc8c731c9a9
Souvikns <souvikde.ns@gmail.com>
2025-06-14 16:25:17 +0530

    feat: connect supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+Future<void> main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
 
-void main() {
+  await Supabase.initialize(
+    url: dotenv.get("SUPABASE_URL"),
+    anonKey: dotenv.get("SUPABASE_ANON_KEY")
+  );
   runApp(const MyApp());
 }
 
